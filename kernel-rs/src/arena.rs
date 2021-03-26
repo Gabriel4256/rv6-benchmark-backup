82fb06b9302468c1f042d921f3e590d1c9b327e3
travis1829 <travis1829@naver.com>
2021-03-26 13:08:52 +0000

    Revert "Support multiple `ListEntry`s in a single node."
    This reverts commit 59cfe8506d9a9709aec428e185a480f0704ed125.

--- kernel-rs/src/arena.rs
+++ kernel-rs/src/arena.rs
@@ -133,7 +133,7 @@ pub struct MruArena<T, const CAPACITY: usize> {
     #[pin]
     entries: [MruEntry<T>; CAPACITY],
     #[pin]
-    list: List<MruEntry<T>, 0>,
+    list: List<MruEntry<T>>,
 }
 
 /// # Safety
@@ -298,7 +298,8 @@ impl<T> MruEntry<T> {
     }
 }
 
-impl<T> ListNode<0> for MruEntry<T> {
+// Safe since `MruEntry` owns a `ListEntry`.
+unsafe impl<T> ListNode for MruEntry<T> {
     fn get_list_entry(&self) -> &ListEntry {
         &self.list_entry
     }
