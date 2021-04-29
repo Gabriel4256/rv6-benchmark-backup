ef5ceca028b06b24601a0074ac95ac459bff6566
Jaemin Hong <hjm0901@gmail.com>
2021-04-29 22:35:19 +0900

    Remove println! macro

--- kernel-rs/src/syscall.rs
+++ kernel-rs/src/syscall.rs
@@ -20,7 +20,6 @@ use crate::{
     ok_or,
     page::Page,
     param::{MAXARG, MAXPATH},
-    println,
     proc::{CurrentProc, KernelCtx},
     some_or,
 };
@@ -124,12 +123,12 @@ impl KernelCtx<'_, '_> {
             22 => self.sys_poweroff(),
             23 => self.sys_clock(),
             _ => {
-                println!(
+                self.kernel().write_fmt(format_args!(
                     "{} {}: unknown sys call {}",
                     self.proc().pid(),
                     str::from_utf8(&self.proc().deref_data().name).unwrap_or("???"),
                     num
-                );
+                ));
                 Err(())
             }
         }
