95906d5feed31ea0c361141bda061642ab3e0990
sunghwan shim <tla4256@gmail.com>
2021-07-29 12:09:51 +0900

    refactoring: divide vm.rs

--- kernel-rs/src/exec.rs
+++ kernel-rs/src/exec.rs
@@ -7,13 +7,8 @@ use itertools::*;
 use zerocopy::{AsBytes, FromBytes};
 
 use crate::{
-<<<<<<< HEAD
-    arch::addr::{pgroundup, PAddr, PGSIZE},
     fs::{FileSystem, FileSystemExt, Path},
-=======
     addr::{pgroundup, PAddr, PGSIZE},
-    fs::{FileSystem, Path},
->>>>>>> refactoring
     hal::hal,
     page::Page,
     param::MAXARG,
