kaist-cp/rv6

Deref, DerefMut for ExecutingProc

kernel-rs/src/console.rs
@@ -61,7 +61,7 @@ impl Console {
             // Wait until interrupt handler has put some
             // input into CONS.buffer.
             while this.r == this.w {
-                if kernel().myexproc().proc().killed() {
+                if kernel().myexproc().killed() {
                     return -1;
                 }
                 this.sleep();

kernel-rs/src/sleeplock.rs
@@ -28,7 +28,7 @@ impl RawSleeplock {
         while *guard != -1 {
             guard.sleep();
         }
-        *guard = unsafe { kernel().myexproc().proc().pid() };
+        *guard = unsafe { kernel().myexproc().pid() };
     }
 
     pub fn release(&self) {
@@ -39,7 +39,7 @@ impl RawSleeplock {
 
     pub fn holding(&self) -> bool {
         let guard = self.locked.lock();
-        *guard == unsafe { kernel().myexproc().proc().pid() }
+        *guard == unsafe { kernel().myexproc().pid() }
     }
 }
 

Gabriel4256/rv6-benchmark-backup

kernel-rs/src/console.rs
@@ -61,7 +61,7 @@ impl Console {
             // Wait until interrupt handler has put some
             // input into CONS.buffer.
             while this.r == this.w {
-                if kernel().myexproc().proc().killed() {
+                if kernel().current_proc().killed() {
                     return -1;
                 }
                 this.sleep();

kernel-rs/src/sleeplock.rs
@@ -28,7 +28,7 @@ impl RawSleeplock {
         while *guard != -1 {
             guard.sleep();
         }
-        *guard = unsafe { kernel().myexproc().proc().pid() };
+        *guard = unsafe { kernel().current_proc().pid() };
     }
 
     pub fn release(&self) {
@@ -39,7 +39,7 @@ impl RawSleeplock {
 
     pub fn holding(&self) -> bool {
         let guard = self.locked.lock();
-        *guard == unsafe { kernel().myexproc().proc().pid() }
+        *guard == unsafe { kernel().current_proc().pid() }
     }
 }
 